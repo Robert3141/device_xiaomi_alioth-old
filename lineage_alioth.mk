f4b7c96d801007ca9c8e761577dbd9bd6715d364
Robert3141 <robert.aries1@gmail.com>
2021-04-23 15:08:38 +0100

    Add audio configs (dont know what they do)

--- lineage_alioth.mk
+++ lineage_alioth.mk
@@ -8,7 +8,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from apollo device
+# Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
 # Inherit some common Lineage stuff.
